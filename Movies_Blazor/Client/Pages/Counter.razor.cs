a0e9819e75620331f022baf687a29985c3dba5da
sabapathygithub <37434203+sabapathygithub@users.noreply.github.com>
2020-02-01 23:52:51 +0530

    Content of Lesson 30 to 37
    Learned calling C# method from Javascrip, Ref directive, Cascading Parameters, Debugging Blazor, Timers & IDisposable.

---
+++ Movies_Blazor/Client/Pages/Counter.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Movies_Blazor.Client.Shared.MainLayout;
+
+namespace Movies_Blazor.Client.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        private static int currentCountStatic = 0;
+
+        [Inject]
+        public SingletonService Singleton { get; set; }
+        [Inject]
+        public TransientService Transient { get; set; }
+        [Inject]
+        public IJSRuntime JS { get; set; }
+        [CascadingParameter]
+        public AppState AppState { get; set; }
+
+        [JSInvokable]
+        public async Task IncrementCount()
+        {
+            currentCount++;
+            Singleton.Value++;
+            Transient.Value++;
+            currentCountStatic++;
+            await JS.InvokeVoidAsync("dotnetStaticInvocation");
+        }
+
+        public async Task IncrementCountJavaScript()
+        {
+            await JS.InvokeVoidAsync("dotnetInstanceInvocation",
+                DotNetObjectReference.Create(this));
+        }
+
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
+        }
+    }
+}
--- Movies_Blazor/Client/wwwroot/js/Utilities.js
+++ Movies_Blazor/Client/wwwroot/js/Utilities.js
@@ -1,3 +1,14 @@
 ﻿function my_function(message) {
     console.log("From utilities : " + message);
 }
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("Movies_Blazor.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript :" + result);
+        });
+}
+
+function dotnetInstanceInvocation(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
